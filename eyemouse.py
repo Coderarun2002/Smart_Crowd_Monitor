2c0e8bde54d02389d5bf121c886862be77678c65
Coderarun2002 <84396398+Coderarun2002@users.noreply.github.com>
2023-07-11 10:37:31 +0530

    Update eyemouse.py

--- eyemouse.py
+++ eyemouse.py
@@ -1,39 +1,46 @@
 import cv2
-import mediapipe as mp
-import pyautogui
-cam = cv2.VideoCapture(0)
-face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
-screen_w, screen_h = pyautogui.size()
-while True:
-    _, frame = cam.read()
-    frame = cv2.flip(frame, 1)
-    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    output = face_mesh.process(rgb_frame)
-    landmark_points = output.multi_face_landmarks
-    frame_h, frame_w, _ = frame.shape
-    key=cv2.waitKey(1)
-    if landmark_points:
-        landmarks = landmark_points[0].landmark
-        for id, landmark in enumerate(landmarks[474:478]):#fixing landmarks for motion
-            x = int(landmark.x * frame_w)
-            y = int(landmark.y * frame_h)
-            cv2.circle(frame, (x, y), 3, (0, 255, 0))
-            if id == 0:
-                screen_x = screen_w * landmark.x
-                screen_y = screen_h * landmark.y
-                pyautogui.moveTo(screen_x, screen_y)
-        left = [landmarks[145], landmarks[159]]
-        right = [landmarks[455], landmarks[457]]
-        
-        if(right[1].y-right[0].y>0.01):
-            pyautogui.click(button='right')
-        for landmark in left:
-            x = int(landmark.x * frame_w)
-            y = int(landmark.y * frame_h)
-            cv2.circle(frame, (x, y), 3, (0, 255, 255))
-            if(left[0].y-left[1].y <0.014):
-                pyautogui.click()
-                pyautogui.sleep(1)
-            if (key==27):
+import smtplib
+import datetime
+n=int(input("Enter the limit"))
+
+trainedmodel=cv2.CascadeClassifier("C:\\Users\\Admin\\AppData\\Local\\Programs\\Python\\Python39\\Lib\\site-packages\\cv2\data\\haarcascade_frontalface_default.xml")
+webcam=cv2.VideoCapture(0)
+i=0
+j=0
+while 1:
+        i=0
+        wc,v=webcam.read()#True or false along with the video of video running or not
+        bnw=cv2.cvtColor(v,cv2.COLOR_BGR2GRAY)
+        face=trainedmodel.detectMultiScale(bnw)#detect the face in cascade classifier
+        for(x,y,w,h) in face:
+            cv2.rectangle(v,(int (x),int (y)),(int (x+w),int(y+h)),(178,123,245),2)#(img,pt1,pt2,color,thickness)
+            cv2.imshow("arunachalam",v)       
+            key=cv2.waitKey(1)
+            i=i+1
+            if key==27:#escape key
                 quit()
-    cv2.imshow('Eye Controlled Mouse', frame)
+            if i>n:
+                if(key==65):#CAPITAL A
+                        n1=len(face)
+                        strTime = datetime.datetime.now().strftime("%H:%M:%S")
+                        print(f"person limit exceeded at the time of {strTime} and the count=",n1)                                        
+            if(key==66):#capital B
+                    strTime = datetime.datetime.now().strftime("%H:%M:%S")
+                    n2=len(face)
+                    print(f"time ={strTime} and number of persons=",n2)
+        if(i>n and j!=(len(face))):
+                l=len(face)
+                r=str(l)
+                import smtplib
+                mymail="arunachalamgurusamy2002@gmail.com"
+                mypass="czyydiywbwrgxnjo"
+                con=smtplib.SMTP("smtp.gmail.com",587)
+                con.starttls()
+                con.login(user=mymail,password=mypass)
+                con.sendmail(from_addr=mymail,to_addrs="arungurusamy2002@gmail.com",msg=f"Person has been exceeded beyond the limit and the count is{r}")
+                con.close()
+                j=len(face)
+
+                
+           
+            
